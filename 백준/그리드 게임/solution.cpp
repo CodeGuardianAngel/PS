22ecf9b439c398c0206f888bb6d08ee1a68797c1
CodeGuardianAngel <114296778+CodeGuardianAngel@users.noreply.github.com>
2022-10-15 09:37:59 +0900

    Update solution.cpp

--- 백준/그리드 게임/solution.cpp
+++ 백준/그리드 게임/solution.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 struct Pos
 {
-	int y, x;
+	int x, y;
 };
 
-const int MAX = 1600 * 2;
-char visit[MAX][MAX];
+const int MAX = 2000 * 2;
+
+bool visit[MAX][MAX];
 
 int main()
 {
@@ -17,29 +18,27 @@ int main()
 	cin.tie(0); cout.tie(0);
 
 	int N, M, K;
-
 	cin >> N >> M;
 
-	Pos start = { 1600, 1600 };
+    Pos start = { 2000,2000 };
 	queue<Pos> q;
 
 	char ch;
-	for (int y = 0; y < N; ++y)
+	for (int y = 0; y < N; y++)
 	{
-		for (int x = 0; x < M; ++x)
+		for (int x = 0; x < M; x++)
 		{
 			cin >> ch;
-			
+			Pos p = { start.x + x, y + start.y };
+
 			if (ch == 'o')
 			{
-				Pos p = { y + start.y, x + start.x };
-                visit[p.y][p.x] = 1;
+				visit[p.y][p.x] = 1;
 				q.push(p);
 			}
 		}
 	}
 
-	// 마지막 줄에는 K (1 ≤ K ≤ 1500)가 주어진다.
 	cin >> K;
 
 	const int dir[4][2] = {
@@ -50,39 +49,25 @@ int main()
 	int ret = 0;
 	while (K--)
 	{
-		int count = q.size()+1;
-		ret += count-1;
-		while(--count)
+		int count = q.size();
+		ret += count;
+
+		while (count--)
 		{
 			Pos cur = q.front();
 			q.pop();
-			
-			Pos next = { cur.y + dir[0][0], cur.x + dir[0][1] };
-			if (visit[next.y][next.x] == 0) {
-				visit[next.y][next.x] = 1;
-				q.push(next);
-			}
 
-			next = { cur.y + dir[1][0], cur.x + dir[1][1] };
-			if (visit[next.y][next.x] == 0) {
-				visit[next.y][next.x] = 1;
-				q.push(next);
-			}
-
-			next = { cur.y + dir[2][0], cur.x + dir[2][1] };
-			if (visit[next.y][next.x] == 0) {
-				visit[next.y][next.x] = 1;
-				q.push(next);
-			}
-
-			next = { cur.y + dir[3][0], cur.x + dir[3][1] };
-			if (visit[next.y][next.x] == 0) {
-				visit[next.y][next.x] = 1;
-				q.push(next);
+			for (int d = 0; d < 4; d++)
+			{
+				Pos next = { cur.x + dir[d][1], cur.y + dir[d][0] };
+				if (visit[next.y][next.x] == 0) {
+					visit[next.y][next.x] = 1;
+					q.push(next);
+				}
 			}
 		}
 	}
 
 	ret += q.size();
-	cout << ret << "\n";
+	cout << ret;
 }
