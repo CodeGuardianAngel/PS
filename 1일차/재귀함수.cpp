f6073fcd3be1b1bddc8a63485825f6248072936b
CodeGuardianAngel <114296778+CodeGuardianAngel@users.noreply.github.com>
2022-10-24 15:16:10 +0900

    Update 재귀함수.cpp

--- 1일차/재귀함수.cpp
+++ 1일차/재귀함수.cpp
@@ -1,135 +1,124 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-const int MAX = 100'000;
+/*
+	n = 1 -> 1
+	sum[n] = sum[n - 1] + n;
 
-int origin[MAX];
-int arr1[MAX];
-int arr2[MAX];	// 4 * 100'000 byte = 390.625 Kbyte = 0.38 MByte
-
-void selection_sort(int arr[], int size)
+	sum[n] = sum[n + 1] + n;
+*/
+int sum_func(int n)
 {
-	// Big(N * N) = O(N^2) => 100'000 * 100'000 = 100억 -> (10억 1초)
-	// N개
-	for (int i = 0; i < size; i++)
-	{
-		// 5개
-		int min_val = arr[i];
-		int min_idx = i;
+	if (n == 1)
+		return 1;
+	return sum_func(n - 1) + n;
+}
 
-		// N - 1 => N
-		for (int j = i + 1 ; j < MAX; j++)
+int sum_func2(int n, int N)
+{
+	if (n == N)
+		return n;
+	return sum_func2(n + 1, N) + n;
+}
+
+/*
+	n! = 1 x ... x n
+
+	fact[n] = fact[n - 1] * n
+
+	n == 1 -> 1
+*/
+int factorial(int n)
+{
+	if (n == 1)
+		return 1;
+	return factorial(n - 1) * n;
+}
+
+/*
+	a^N % 1'000'000'007를 구하라
+	제약 조건 : a는 1~100, N은 1~100억
+	1초 이내 동작
+
+	pow[N] = pow[N - 1] * a
+	N == 1 => a
+
+	100억 => 10억 1초 => 10초
+*/
+const int MOD = 1e9 + 7;
+int pow(int a, int n)
+{
+	if (n == 1)
+		return a;
+	return (pow(a, n - 1) * a) % MOD;
+}
+
+/*
+	N 짝수
+		- a^N = (a*a)^(N/2)
+
+	N 홀수
+		- a^N = (a*a)^(N/2) * a
+
+	정수 / 정수 = 정수
+	11 / 2 = 10
+*/
+int pow2(int a, int n)
+{
+	if (n == 1)
+		return a;
+
+	int b = a * a % MOD;
+	if (n % 2 == 0)
+	{
+		return (pow2(b, n / 2)) % MOD;
+	}
+	else
+	{
+		return (pow2(b, n / 2) * a) % MOD;
+	}
+}
+
+int pow3(int a, int n)
+{
+	int ret = 1;
+
+	while (n)
+	{
+		if (n % 2 == 1)
 		{
-			// 4개
-			if (min_val > arr[j])
-			{
-				min_val = arr[j];
-				min_idx = j;
-			}
+			ret = ret * a;
 		}
-
-		int temp = arr[i];
-		arr[i] = arr[min_idx];
-		arr[min_idx] = temp;
+		a = a * a;
+		n /= 2;
 	}
+	return ret;
 }
-
-int temp_arr[MAX];
-// O(N Log N) => 100'000 * 17 = 1,700,000
-void merge(int arr[], int left, int right)
-{
-	int mid = (left + right) / 2;
-	int k = left;
-	int i = left, j = mid + 1;
-	int e1 = mid;
-	int e2 = right;
-
-	while (i <= e1 && j <= e2)
-	{
-		if (arr[i] > arr[j])
-		{
-			temp_arr[k] = arr[j];
-			j++;
-		}else 
-		{
-			temp_arr[k] = arr[i];
-			i++;
-		}
-		k++;
-	}
-
-	while (i <= e1)
-	{
-		temp_arr[k] = arr[i];
-		i++;
-		k++;
-	}
-
-	while (j <= e2)
-	{
-		temp_arr[k] = arr[j];
-		j++;
-		k++;
-	}
-
-	for (int i = left; i <= right; i++)
-	{
-		 arr[i] = temp_arr[i];
-	}
-}
-
-void divide(int arr[], int left,  int right)
-{
-	if (left < right)
-	{
-		int mid = (left + right) / 2;
-		divide(arr, left, mid);
-		divide(arr, mid + 1, right);
-		merge(arr, left, right);
-	}
-}
-
-void merge_sort(int arr[], int size)
-{
-	divide(arr, 0, size - 1);
-}
-
 
 int main()
 {
-	for (int i = 0; i < MAX; i++)
+#if 0
+	int N = 100;
+	int sum = 0;
+	for (int i = 1; i <= N; i++)
 	{
-		origin[i] = rand() % 100;
-		arr1[i] = arr2[i] = origin[i];
-		// printf("%d ", origin[i]);
+		sum += i;
 	}
-	printf("\n");
 
-	time_t pre, end;
-	printf("=== selection sort ===\n");
-	pre = clock();
-	// selection_sort(arr1, MAX);
-	end = clock() - pre;
-	printf("%d\n", end);
+	/*
+		int -> 4byte = 32 bit 2^32 => (-2^31 ~ 2^31 - 1)
+	*/
 
+	printf("%d %d %d", sum, sum_func(N), sum_func2(1, N));
 
-
-	printf("%d", sizeof(arr1));
-
-	//for (int i = 0; i < MAX; i++)
-	//{
-	//	printf("%d ", arr1[i]);
-	//}
-	
-	printf("\n");
-	printf("=== merge sort ===\n");
-	pre = clock();
-	merge_sort(arr2, MAX);
-	end = clock() - pre;
-	printf("%d\n", end);
-	/*for (int i = 0; i < MAX; i++)
+	int fac = 1;
+	N = 10;
+	for (int i = 1; i <= N; i++)
 	{
-		printf("%d ", arr2[i]);
-	}*/
+		fac = fac * i;
+	}
+
+	printf("%d %d", fac, factorial(N));
+#endif
+
+	printf("%d", pow3(2, 10));
 }
